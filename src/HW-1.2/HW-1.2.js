228ba9e12c228964c7744911de960f4bb06b7053
Matviienko Andrii <matviienkoandrii02@gmail.com>
2024-01-26 15:35:23 +0200

    HW-1.2 fixed

--- src/HW-1.2/HW-1.2.js
+++ src/HW-1.2/HW-1.2.js
@@ -1,19 +1,26 @@
-function canSmoke(age){
-    if(age >= 18){
+function canSmoke(age) {
+    if (age >= 18) {
         return true
     }
     return false
 }
 
-let canSmokeVariable = function(age){
-    if(age >= 18){
+let canSmokeVariable = function (age) {
+    if (age >= 18) {
         return true
     }
     return false
 }
 
 let canSmokeArrow = (age) => {
-    if(age >= 18){
+    if (age >= 18) {
+        return true
+    }
+    return false
+}
+
+function allowedTOIn(name, age, department) {
+    if (name == 'Andrii' && age >= 18 && department == 'QA') {
         return true
     }
     return false
@@ -25,3 +32,7 @@ console.log(canSmokeVariable(14))
 console.log(canSmokeVariable(20))
 console.log(canSmokeArrow(14))
 console.log(canSmokeArrow(20))
+console.log(allowedTOIn("John", 17, "PM"))
+console.log(allowedTOIn("Andrii", 21, 'QA'))
+
+
